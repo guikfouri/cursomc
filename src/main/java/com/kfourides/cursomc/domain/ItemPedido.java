e09a77fb6551597ff8f58f0edd47ae82b0e9e4e7
Guilherme Kfouri <guiokfouri@gmail.com>
2021-02-24 17:22:14 -0300

    Endpoint /pedidos/{id}

--- src/main/java/com/kfourides/cursomc/domain/ItemPedido.java
+++ src/main/java/com/kfourides/cursomc/domain/ItemPedido.java
@@ -5,10 +5,13 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido  implements Serializable{
 	private static final long serialVersionUID = 1L;
 
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -17,7 +20,6 @@ public class ItemPedido  implements Serializable{
 	private Double preco;
 	
 	public ItemPedido() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ItemPedido(Pedido pedido, Produto produto, Double desconto, Integer quantidade, Double preco) {
@@ -29,6 +31,7 @@ public class ItemPedido  implements Serializable{
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
